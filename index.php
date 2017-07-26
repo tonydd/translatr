d6e47fafb16d55286f2c8fd296c2204b1bfa20fe
Anthony Den Drijver <anthodd@gmail.com>
2017-07-26 20:53:55 +0200

    INIT

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html class="no-js" lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Titre du document</title>
+    <link rel="stylesheet" href="css/knacss.css" media="all">
+    <link rel="stylesheet" href="css/styles.css" media="all">
+  </head>
+  <body>
+    
+  <header id="header" role="banner" class="pam">
+    Interface de gestion des traductions
+  </header>
+  <div class="flex-container">
+    <aside class="mod w20 pam aside">
+      <nav id="navigation" role="navigation">
+        <ul class="pam">
+          <li class="pam inbl">Picon bière</li>
+          <li class="pam inbl">Melfor</li>
+          <li class="pam inbl">Carola</li>
+          <li class="pam inbl">Kuglof</li>
+          <li class="pam inbl">Wurscht</li>
+        </ul>
+      </nav>
+      <p>Lorem Salu bissame ! Wie geht's les samis ? Hans apporte moi une Wurschtsalad avec un picon bitte, s'il te plaît. Voss ? Une Carola et du Melfor ? Yo dû, espèce de Knäckes, ch'ai dit un picon !</p>
+    </aside>
+    <main id="main" role="main" class="flex-item-fluid pam">
+
+    </main>
+  </div>
+
+  <script type="text/javascript" src="libs/jquery-3.2.1.min.js"></script>
+  </body>
+</html>
